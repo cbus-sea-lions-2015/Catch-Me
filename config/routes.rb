ce499a155846a311c717969a1ddac5280420514a
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-16 23:37:29 -0400

    add login/logout in layout, add custom logout route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   root 'location_details#index'
 
   get '/all_locations_for_map' => 'location_details#all_locations_for_map'
+  get '/logout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
