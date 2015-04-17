2b25ffcae9aac71e451f64172bc09bac4d71a899
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-16 21:55:50 -0400

    add bcrypt helper in User, add User controller actions for new/create and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.update(user_signup_params)
+    if @user.save
+      redirect_to @user
+    else
+      redirect_to :new, flash: "You screwed up"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def user_signup_params
+    params.require(:user).permit(:name,:username,:password,:handle)
+  end
+
+  def user_profile_update_params
+    params.require(:user).permit(:name,:handle,:about_me)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :routes, foreign_key: "creator_id"
 	has_many :activities
+
+  has_secure_password
 end
